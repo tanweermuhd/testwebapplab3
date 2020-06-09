dec5c8b623cb9d5b85de1fef24deb40f3f3e65bb
tanweermuhd <53080788+tanweermuhd@users.noreply.github.com>
2020-06-09 23:14:05 +0800

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $serverName= "testdbtp045897.database.windows.nett";
 
 $connectionOptions = array (
@@ -9,7 +10,10 @@ $connectionOptions = array (
 
 //establishes the connection
 
-$conn = sqlsrv_connect($serverName, $connectionOptions);
+$conn = sqlsrv_connect($serverName, $connectionOptions); */
+$connectionInfo = array("UID" => "apuadmin", "pwd" => "Apu123456", "Database" => "testdbtp045897", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:testdbtp045897.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 if(!$conn)
 {
 die ("Error connection: ".sqlsrv_errors());
